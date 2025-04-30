e5b53c8de4060a65692b543fdb9aeca71259573b
marlonlambda <marlon.jaramillo@lambdaanalytics.co>
2025-04-30 12:36:47 -0500

    feat(auth): agrega configuracion inicial del modulo de autenticacion

---
+++ auth/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import User
+
+class UserLoginSerializer(serializers.Serializer):
+    correo = serializers.CharField(max_length=100, required=False)
+    password = serializers.CharField(max_length=100, required=False)
+
+    def validate(self, data):
+        correo = data.get('correo', None)
+        password = data.get('password', None)
+
+        if not correo or not password:
+            raise serializers.ValidationError({"error": "Debe proporcionar un correo electrónico y una contraseña."})
+
+        # Convertir el correo a minúsculas antes de la búsqueda
+        correo = correo.lower()
+        print(correo)
+        try:
+            user = User.objects.get(correo=correo)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({"error": "No se encontró un usuario con este correo electrónico."})
+
+        if not user.check_password(password):
+            raise serializers.ValidationError({"error": "La contraseña proporcionada es incorrecta."})
+
+        if not user.is_active:
+            raise serializers.ValidationError({"error": "Su cuenta está inactiva, contacte al administrador."})
+
+        data['correo'] = user 
+        return data
